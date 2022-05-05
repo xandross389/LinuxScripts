4e9641bf7c616dc8c7a8dc7248eef7c6c65e2996
XandrOSS <67112856+xandross389@users.noreply.github.com>
2022-05-05 16:54:54 -0400

    install_and_run_ytadsblocker.sh
    install and run ytadsblocker.sh in pihole docker container

---
+++ install_and_run_ytadsblocker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# to run execute: 
+# chmod +x ./install_and_run_ytadsblocker.sh
+# inside docker container> ./install_and_run_ytadsblocker.sh
+# asumming you have mapped script to /usr/src from outside docker container run: sudo sudo docker exec -it pihole /usr/src/install_and_run_ytadsblocker.sh
+apt update -qq && apt install -qq systemctl -y
+#cd /usr/src/
+curl https://raw.githubusercontent.com/deividgdt/ytadsblocker/master/ytadsblocker.sh -o /usr/src/ytadsblocker.sh
+chmod +x /usr/src/ytadsblocker.sh
+/usr/src/ytadsblocker.sh -a stop
+/usr/src/ytadsblocker.sh -a uninstall
+/usr/src/ytadsblocker.sh -a install
+/usr/src/ytadsblocker.sh -a start &
