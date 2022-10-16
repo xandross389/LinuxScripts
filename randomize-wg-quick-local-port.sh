88b8c5667b1588d38d8772050a94cf6a5bd8eec3
XandrOSS <67112856+xandross389@users.noreply.github.com>
2022-10-16 11:39:25 -0400

    Update randomize-wg-quick-local-port.sh

--- randomize-wg-quick-local-port.sh
+++ randomize-wg-quick-local-port.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Modifies /usr/bin/wg-quick script to randomize wireguard client local port created by wg-quick. 
+# To rollback to original script use randomize-wg-quick-local-port.sh
 # Run as root or sudoer user
 sed -i 's/table=51820/table=$(shuf -i 2000-65000 -n 1) #table=51820/g' /usr/bin/wg-quick
